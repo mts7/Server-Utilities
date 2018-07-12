2a44f002ca751407410186ef43a301f50536470d
Mike Rodarte <mike.rodarte@bio-techne.com>
2018-07-12 13:29:27 -0600

    created script to find all git repositories

---
+++ find-git-dirs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Find all directories named .git (which should be only git repositories)
+# Ignore anything in /mnt/
+
+find / -type d -name ".git" 2> >(grep -Ev 'Permission denied|No such file or directory' >&2) | grep -v '/mnt/'
+
