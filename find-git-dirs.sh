507b263910ad5bcb101fa807987afb975491c6dc
Mike Rodarte <mike.rodarte@bio-techne.com>
2018-07-27 09:27:11 -0600

    added file system parameter to server-check and removed mnt restriction from git dirs

--- find-git-dirs.sh
+++ find-git-dirs.sh
@@ -2,5 +2,5 @@
 # Find all directories named .git (which should be only git repositories)
 # Ignore anything in /mnt/
 
-find / -type d -name ".git" 2> >(grep -Ev 'Permission denied|No such file or directory' >&2) | grep -v '/mnt/'
+find / -type d -name ".git" 2> >(grep -Ev 'Permission denied|No such file or directory' >&2) 
 
